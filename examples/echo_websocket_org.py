2152a685371adf561448f023b091c85859cfd77e
HermannSW <hermannstamm-wilbrandt.de>
2018-10-16 02:14:54 +0200

    Modify new example according danni's remarks

--- examples/echo_websocket_org.py
+++ examples/echo_websocket_org.py
@@ -1,9 +1,7 @@
 import uwebsockets.client
-
 websocket = uwebsockets.client.connect("ws://echo.websocket.org/")
-
-websocket.send("fo0bAr\r\n")
-
+mesg = "The quick brown fox jumps over the lazy dog"
+websocket.send(mesg + "\r\n")
 resp = websocket.recv()
-
 print(resp)
+assert(mesg + "\r\n" == resp)
